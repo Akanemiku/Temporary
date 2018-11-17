c0cc654b2047cd8833d4d2cc89ea91d069b8273b
Akanemiku <33932911+Akanemiku@users.noreply.github.com>
2018-11-17 20:12:28 +0800

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,112 +1,168 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string.h>
 using namespace std;
+
 class Birthday
 {
-    private:
-        int day_;
-        int month_;
-        int year_;
-    public:
-        Birthday(){}
-        Birthday(int day,int month,int year)
-        {
-            day_ = day;
-            month_ = month;
-            year_ = year;
-        }
-        void Print()
-        {
-            cout<<"Birthday:"<<year_<<" "<<month_<<" "<<day_<<endl;
-        }
+	public:
+		Birthday(){}
+		Birthday(int year, int month, int day)
+		{
+			cout<<"Birthday has been build."<<endl;
+			year_ = year;
+			month_ = month;
+			day_ = day;
+		}
+		void Print()
+		{
+			cout<<year_<<"/"<<month_<<"/"<<day_<<endl;
+		}
+		
+	private:
+		int year_;
+		int month_;
+		int day_;
 };
+
+enum Gender
+{
+	Male,
+	Female
+};
+std::ostream& operator<<(std::ostream& os, Gender gender)
+{
+	switch (gender)  
+    {  
+    case Male:  
+        os << "Male";  
+        break;  
+    case Female:  
+        os << "Female";  
+        break; 
+	}
+	return os;	
+} 
 class Person
 {
-public:
-    enum Gender { Male,  Female };
-    Person(){}
-    Person(Birthday birth,Gender gender,string name)
-    {
-        birth_ = birth;
-        gender_ = gender;
-        name_ = name;
-    }
-    void Print()
-    {
-        cout<<"Name:"<<name_<<
-        " Gender:"<<gender_<<endl;
-        birth_.Print();
-
-    }
-private:
-    Birthday birth_;
-    Gender gender_;
-    string name_;
+	public:
+		Person(){}
+		Person(char* name, Gender gender, Birthday birth)
+		{
+			cout<<"Person has been build."<<endl;
+			name_ = new char[strlen(name) + 1];
+			strcpy(name_, name);
+			gender_ = gender;
+			birth_ = birth;
+			
+		}
+		void Print()
+		{
+			cout<<"Name:"<<name_<<endl;
+			cout<<"Gender:"<<gender_<<endl;
+			cout<<"Birthday:";
+			birth_.Print();
+		}
+		
+	private:
+		char* name_;
+		Gender gender_;
+		Birthday birth_;
 };
+
 class Teacher:virtual public Person
 {
-public:
-    Teacher(){}
-    Teacher(Birthday birth,Gender gender,string name,string title)
-    :Person(birth,gender,name)
-    {
+	public:
+		Teacher(){}
+		Teacher(char* name, Gender gender, Birthday birth, string title)
+		{
+			Person(name, gender, birth);
+			cout<<"Teacher has been build."<<endl;
+			title_ = title;
+		}
+		void Print()
+		{
+			Print();
+			cout<<"Title:"<<title_<<endl;
+		}
+		void setTitle(string title)
+		{
+			title_ = title;
+		}
+		string getTitle()
+		{
+			return title_;
+		}
 
-    }
-    void Print()
-    {
-
-    }
-private:
-    string title_;
+	private:
+		string title_;
 };
+
 class Student:virtual public Person
 {
-public:
-    Student(){}
-    Student(Birthday birth,Gender gender,string name,int score)
-    :Person(birth,gender,name)
-    {
-
-    }
-    void Print()
-    {
-
-    }
-private:
-    int score_;
+	public:
+		Student(){}
+		Student(char* name, Gender gender, Birthday birth, int score):Person(name, gender, birth)
+		{
+			cout<<"Student has been build."<<endl;
+			score_ = score;
+		}
+		void Print()
+		{
+			Person::Print();
+			cout<<"Score:"<<score_<<endl;
+		}
+	private:
+		int score_;
 };
+
 class Graduate:public Student
 {
-public:
-    Graduate(){}
-    Graduate(Birthday birth,Gender gender,string name,int score,string advisor):Student(birth,gender,name,score)
-    {
-
-    }
-    void Print()
-    {
-
-    }
-private:
-    string advisor_;
+	public:
+		Graduate()
+		{
+		}
+		Graduate(char* name, Gender gender, Birthday birth, int score, string advisor):Student(name, gender, birth, score),Person(name, gender, birth)
+		{
+			cout<<"Graduate has been build."<<endl;
+			advisor_ = advisor;
+		}
+		void Print()
+		{
+			Student::Print();
+			cout<<"Advisor:"<<advisor_<<endl;
+		}
+	private:
+		string advisor_;
 };
 
-class Assistant:public Graduate,public Teacher
+class Assistant:public Graduate, public Teacher
 {
-public:
-    Assistant(){}
-    Assistant(Birthday birth,Gender gender,string name,int score,string advisor,string title)
-    :Graduate(birth,gender,name,score,advisor)
-    ,Teacher(birth,gender,name,title)
-    {
-        
-    }
-private:
-    string subject_;
+	public:
+		Assistant()
+		{
+		}
+		Assistant(char* name, Gender gender, Birthday birth, int score, string advisor, string title, string subject):Graduate(name, gender, birth, score, advisor),Teacher(name, gender, birth, title),Person(name, gender, birth) 
+		{
+			cout<<"Assistant has been build."<<endl;
+			Teacher::setTitle(title);
+			subject_ = subject;
+		}
+		void Print()
+		{
+			Graduate::Print();	
+			cout<<"Title:"<<Teacher::getTitle()<<endl;
+			cout<<"Subject:"<<subject_;
+		}
+	private:
+		string subject_;
 };
 
 int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+{ 	
+	Birthday birth(1997, 9, 15);
+	Assistant assistant("Rory", Male, birth, 100, "mengjie Ye", "Senior engineer", "Computer science");
+	assistant.Print();
+	
+	
+	return 0;
 }
